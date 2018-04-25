5c0a52f0b149c4a447f8e5dd91509fa1fc089ad2
lukinev <lukinev@ukr.net>
2018-04-26 01:30:06 +0300

    1

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ let cron = require('cron');
 const fs = require('fs');
 const { promisify } = require('util');
 const readFileAsync = promisify(fs.readFile);
-
 let jobs = [];
 
-async function getJobs() {
+async function fnc() {
+    let arr = [];
     try {
-        const tasksList = await readFileAsync('tasksList.json', { encoding: 'utf8' });
-        console.log('CONTENT:', tasksList);
+        let tasksArray = JSON.parse(await readFileAsync('tasksList.json', { encoding: 'utf8' })).tasksList;
+        for (let index = 0; index < tasksArray.length; index++) {
+            arr.push(new cron.CronJob({
+                cronTime: '*/1 * * * * ',
+                onTick: function () {
+
+                    console.log('job ticked');
+                },
+                onComplete: function () {
+                    console.log('All done');
+                },
+                start: false,
+                timeZone: 'Europe/Kiev'
+            }));
+        }        
+        return arr;
     }
     catch (err) {
         console.log('ERROR:', err);
     }
+};
 
+jobs = fnc().slice;
+
+
+for (let index = 0; index < jobs.length; index++) {
+    jobs[index].start();
 }
-
-jobs.push(new cron.CronJob({
-    cronTime: '*/1 * * * *',
-    onTick: function () {
-        getJobs();
-        console.log('job 1 ticked');
-    },
-    onComplete: function () {
-        console.log('All done');
-    },
-    start: false,
-    timeZone: 'Europe/Kiev'
-}));
-jobs[0].start(); // job 1 started
